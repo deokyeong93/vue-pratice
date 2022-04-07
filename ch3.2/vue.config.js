db93393cd598f1390644942e6fb4655697954ddf
rencarHDH <deokyeong1020@rencar.co.kr>
2022-04-07 13:26:39 +0900

    Add : Vue Router 직접 추가하여 이것저것 해보기

--- ch3.2/vue.config.js
+++ ch3.2/vue.config.js
@@ -1,4 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  chainWebpack: config => {
+    // 우선 기본적으로 prefetch를 비활성화 한 후에
+    // 각각의 lazyLoad에서 prefetch할지말지 결정하는 것이 좋다.
+    config.plugin.delete('prefetch');
+  }
+});
