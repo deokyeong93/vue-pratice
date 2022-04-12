6f876e3540bd8999de9e8186aa4ca1ea651ced59
deokyeong93 <deokyeong1020@gmail.com>
2022-04-12 11:39:00 +0900

    props 동적으로 넘겨보기

--- ch3.2/src/components/PageTitle.vue
+++ ch3.2/src/components/PageTitle.vue
@@ -1,13 +1,37 @@
 <template>
-    <h2 class='text-4xl'>{{title}}</h2>
+  <h2 class="text-4xl">{{ title }}</h2>
+  <div>{{ numberProps }}</div>
 </template>
 <script>
 export default {
-    props: {
-        title : {
-            type : String,
-            default: '페이지 제목을 써주세요'
-        }
-    }
-}
+  props: {
+    title: {
+      type: String,
+      default: "페이지 제목을 써주세요",
+    },
+    // numberProps: Number,
+    // propsC: {
+    //   type: String,
+    //   required: true,
+    // },
+    // propsObj: {
+    //   type: Object,
+    //   default() {
+    //     return { message: "message" };
+    //   },
+    // },
+    // propsVal: {
+    //   validator(value) {
+    //     console.log("propsVal=>", value);
+    //     return ["success", "warning", "danger"].indexOf(value) !== -1;
+    //   },
+    // },
+    // propsFunc: {
+    //   type: Function,
+    //   default() {
+    //     return "Default Function";
+    //   },
+    // },
+  },
+};
 </script>
--- ch3.2/src/views/AboutView.vue
+++ ch3.2/src/views/AboutView.vue
@@ -1,15 +1,18 @@
 <template>
-  <PageTitle title='전달하는 값이야'/>
+  <PageTitle title="전달하는 값이야" />
+  <!-- 이하는 동적인 전달을 해보자 -->
+  <PageTitle :title="title" />
+  <!-- 숫자형을 전달해보자 -->
 </template>
 <script>
-import PageTitle from '../components/PageTitle.vue'
+import PageTitle from "../components/PageTitle.vue";
 
 export default {
   name: "sample-",
-  components: {PageTitle},
+  components: { PageTitle },
   data() {
     return {
-      sampleDate: "",
+      title: "동적으로 전달해보자",
     };
   },
   beforeCreate() {},
