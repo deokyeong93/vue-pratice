a3ed2a3e99d8c34054525e5f30cc38cbd2c28842
deokyeong93 <deokyeong1020@gmail.com>
2022-04-16 16:18:06 +0900

    new init

---
+++ ex2/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+  },
+};
